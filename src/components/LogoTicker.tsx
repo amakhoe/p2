40b6d3e96134cc5c6fb4ece7d195333e2a713148
amakhoe <67333002+amakhoe@users.noreply.github.com>
2025-02-14 00:27:52 +0400

    Update LogoTicker.tsx

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -20,7 +20,7 @@ export const LogoTicker = () => {
   return (
     <div className="bg-black text-white py-[72px] ">
       <div className="container">
-        <h2 className="text-xl text-center text-white/70">Confiado por grandes empresas</h2>
+        <h2 className="text-xl text-center text-white/70 text-5xl">Confiado por grandes empresas</h2>
       <div className="overflow-hidden mt-9 before:content-[''] before:z-10 before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative  before:left-0 before:top-0 after:top-0 after:bg-[linear-gradient(to_right, #000, rgb(0,0,0,0))] before:bg-[linear-gradient(to_right, #000, rgb(0,0,0,0))]">
         <motion.div className="flex gap-16 pr-16 flex-none" 
         animate={{
